bcc70290eae857eb608b14ce654e032a89711dff
Wesley <wj.schoonbeek@gmail.com>
2019-10-11 13:40:48 +0200

    dasdsa
    dsadsadasd

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <body>
   <!-- header row -->
   <div class="row">
-    <div class="col-12 header bg-midgreen">
+    <div class="col-12 header bg-white">
       <div class="row">
         <div class="col-2 logo">
           <!-- <img src="https://s.s-bol.com/imgbase0/imagebase3/large/FC/3/9/8/0/9200000013300893.jpg" /> -->
@@ -24,10 +24,10 @@
         </div>
         <div class="col-10 navbar">
           <ul>
-            <li><a class="active" href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a class="active fnt-black" href="#home">Home</a></li>
+            <li><a href="#news" class="fnt-black">News</a></li>
+            <li><a href="#contact" class="fnt-black">Contact</a></li>
+            <li><a href="#about" class="fnt-black">About</a></li>
           </ul>
         </div>
       </div>
@@ -38,11 +38,13 @@
   <div class="row">
     <div class="col-12 bigimage">
       <div class="row">
-        <div class="col-5"></div>
-        <div class="col-2">
-          <h1 class="white">Innovative, blabla, more blabla</h1>
+        <div class="col-1">
+          <!-- Place holder -->
         </div>
-        <div class="col-5"></div>
+        <div class="col-1">
+          <h1 class="white fnt-white">Innovative, blabla, more blabla</h1>
+        </div>
+        
       </div>
     </div>
   </div>
@@ -50,11 +52,12 @@
   <!-- main page row -->
   <div class="row mainpage">
     <div class="col-2 sidebar">
-      SIDEBAR
+      RSS-FEED
     </div>
     <div class="col-10 content">
       <!-- CONTENT HIER !!  -->
       <div class="row">
+
         <div class="col-3 card">
           <div class="card_title">Vind jouw opleiding</div>
           <p>Maak een keuze uit ons complete aanbod aan opleidingen.</p>
@@ -70,6 +73,13 @@
         <div class="col-3 card">
           <div class="card_title">Hulp bij opleiding kiezen</div>
           <p>Wij helpen je graag om de opleiding te vinden die jou op het lijf geschreven is.</p>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="col-12 card">
+          <h1>Amazon hogeschool. Grensverleggend !</h1>
+          <p>Door je grenzen te verleggen bereik je meer. Daarom dagen onze docenten je iedere dag uit je talenten te ontdekken en te ontwikkelen. Deze ontdekkingsreis overstijgt de grenzen van je opleiding. Je doet namelijk ook kennis op bij andere opleidingen en zelfs buiten de muren van onze hogeschool. Onze campussen vind je in Noord-Nederland en in het buitenland. Van Leeuwarden tot Zuid-Afrika; de wereld ligt aan je voeten.</p>
         </div>
       </div>
     </div>
