a8eb6b96c4487613dafc576df096266f9f9e82b8
Storm-Visser <56340991+Storm-Visser@users.noreply.github.com>
2019-10-15 11:42:39 +0200

    Update opleidingen.php

---
+++ opleidingen.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <!--  Meta  -->
+  <meta charset="UTF-8" />
+  <title>School project template</title>
+
+  <!--  Styles  -->
+  <link rel="stylesheet" href="styles/grid.css">
+  <link rel="stylesheet" href="styles/index.css">
+
+</head>
+
+<body>
+  <!-- header row -->
+  <div class="row">
+    <div class="col-12 header bg-white">
+      <div class="row">
+        <div class="col-2 logo">
+          <!-- <img src="https://s.s-bol.com/imgbase0/imagebase3/large/FC/3/9/8/0/9200000013300893.jpg" /> -->
+          <p>AMAZON</p>
+        </div>
+        <div class="col-10 navbar">
+          <ul>
+            <li><a class="active fnt-black" href="#home">Home</a></li>
+            <li><a href="#news" class="fnt-black">News</a></li>
+            <li><a href="#contact" class="fnt-black">Contact</a></li>
+            <li><a href="#about" class="fnt-black">About</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end header -->
+  <!-- Big image -->
+  <div class="row">
+    <div class="col-12 bigimage">
+      <div class="row">
+        <div class="col-1">
+          <!-- Place holder -->
+        </div>
+        <div class="col-1">
+          <h1 class="white fnt-white">Innovative, blabla, more blabla</h1>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  <!-- end big image -->
+  <!-- main page row -->
+  <div class="row mainpage">
+    <div class="col-2 sidebar">
+      RSS-FEED
+    </div>
+    <div class="col-10 content">
+      <!-- CONTENT HIER !!  -->
+	  <div class="row">
+	  <!--leeg-->
+	  </div>
+
+	  <div class="row">
+		<div class="col-12">
+		<p>Bij Amazon hogeschool bieden wij een aantal opleiding op het gebied van informatica en ICT </p>
+		</div>
+	  </div>
+
+	  <div class="row">
+		<div class="col-6">
+			<a href="gamedesing.php">
+			<img src="gamedesing.jpg"/>
+			</a>
+		</div>
+		<div class="col-6">
+			<a href="systeembeheer.php">
+			<img src="systeembeheer.jpg"/>
+			</a>
+		</div>
+	  </div>
+	  <div class="row">
+		<div class="col-6">
+			<a href="softwareengeneering.php">
+			<img src="softwareengeneering.jpg"/>
+			</a>
+		</div>
+		<div class="col-6">
+			<a href="security.php">
+			<img src="security.jpg"/>
+			</a>
+		</div>
+	  </div>
+    </div>
+  </div>
+  <!-- end main page row -->
+  <!-- Footer -->
+  <div class="row footer bg-midgreen fnt-white">
+    <div class="col-3">
+      <!-- Lege placeholder -->
+    </div>
+    <div class="col-6">
+      Copyright 2019 Amazon hogeschool
+    </div>
+    <div class="col-3">
+      <!-- Lege placeholder -->
+    </div>
+  </div>
+  <!-- end footer -->
+
+  <script src="scripts/index.js"></script>
+</body>
+
+</html>
