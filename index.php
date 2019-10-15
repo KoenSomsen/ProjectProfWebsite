31a1bfacd07ecdf04bf99ab1c3be81bc146ef2cb
Sydney Minnaar <sydney.minnaar@student.nhlstenden.com>
2019-10-15 13:27:31 +0200

    Homepagina verandered
    - Logo toegevoegd
    - Footer tekst veranderd
    - Inspirerende tekst toegevoegd

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
   <!--  Meta  -->
   <meta charset="UTF-8" />
   <title>School project template</title>
@@ -10,7 +8,6 @@
   <!--  Styles  -->
   <link rel="stylesheet" href="styles/grid.css">
   <link rel="stylesheet" href="styles/index.css">
-
 </head>
 
 <body>
@@ -19,8 +16,7 @@
     <div class="col-12 header bg-white">
       <div class="row">
         <div class="col-2 logo">
-          <!-- <img src="https://s.s-bol.com/imgbase0/imagebase3/large/FC/3/9/8/0/9200000013300893.jpg" /> -->
-          <p>AMAZON</p>
+          <img id="logo" src="images/logo.svg" alt="Logo Amazon Hogeschool"/>
         </div>
         <div class="col-10 navbar">
           <ul>
@@ -38,13 +34,15 @@
   <div class="row">
     <div class="col-12 bigimage">
       <div class="row">
-        <div class="col-1">
+		<div class="col-1">
           <!-- Place holder -->
         </div>
-        <div class="col-1">
-          <h1 class="white fnt-white">Innovative, blabla, more blabla</h1>
+        <div class="col-2">
+			<h1 class="white fnt-white">
+				Verleg je eigen grenzen, niet de Amazone.
+			</h1>
         </div>
-        
+        <div class="col-9"></div>
       </div>
     </div>
   </div>
@@ -60,15 +58,15 @@
 
         <div class="col-3 card">
           <div class="card_title">Vind jouw opleiding</div>
-          <p>Maak een keuze uit ons complete aanbod aan opleidingen.</p>
+          <p>Maak een keuze uit één of meer van onze opleidingen</p>
         </div>
         <div class="col-3 card">
           <div class="card_title">Inschrijven: hoe doe je dat?</div>
           <p>Met een inschrijving zet je de eerste stap richting jouw studie bij Amazon Hogeschool.</p>
         </div>
         <div class="col-3 card">
-          <div class="card_title">Infoavond Werken & Studeren</div>
-          <p>Kom naar onze Infoavond deeltijdopleidingen op dinsdag 26 november.</p>
+          <div class="card_title">Informatie avonden</div>
+          <p>Kom naar onze Informatie avond over de opleidingen van Amazon Hogeschool op dinsdag 26 november.</p>
         </div>
         <div class="col-3 card">
           <div class="card_title">Hulp bij opleiding kiezen</div>
@@ -78,7 +76,7 @@
       </div>
       <div class="row">
         <div class="col-12 card">
-          <h1>Amazon hogeschool. Grensverleggend !</h1>
+          <h1>Amazon hogeschool. Grensverleggend!</h1>
           <p>Door je grenzen te verleggen bereik je meer. Daarom dagen onze docenten je iedere dag uit je talenten te ontdekken en te ontwikkelen. Deze ontdekkingsreis overstijgt de grenzen van je opleiding. Je doet namelijk ook kennis op bij andere opleidingen en zelfs buiten de muren van onze hogeschool. Onze campussen vind je in Noord-Nederland en in het buitenland. Van Leeuwarden tot Zuid-Afrika; de wereld ligt aan je voeten.</p>
         </div>
       </div>
@@ -87,19 +85,12 @@
   <!-- end main page row -->
   <!-- Footer -->
   <div class="row footer bg-midgreen fnt-white">
-    <div class="col-3">
-      <!-- Lege placeholder -->
-    </div>
-    <div class="col-6">
-      Copyright 2019 Amazon hogeschool
-    </div>
-    <div class="col-3">
-      <!-- Lege placeholder -->
-    </div>
+	<div class="col-12">
+		© 2019 Amazon hogeschool
+	</div>
   </div>
+  
   <!-- end footer -->
-
   <script src="scripts/index.js"></script>
 </body>
-
 </html>
