4eeac8eee2e90f7b667222bb2ed0b4bd22ef1e2b
KoenSomsen <koen.somsen@student.nhlstenden.com>
2019-10-17 10:39:19 +0200

    rss updates

--- rss.php
+++ rss.php
@@ -11,11 +11,11 @@
 			foreach ($xml->channel->item as $item2){
 				echo "<div class='filler'>"; // volledige blok
 
-				echo "<div class='kleur fontSize'>" .$item2->title . "</div><br>";
+				echo "<div class='kleur-fontSize'>" .$item2->title . "</div><br>";
                       
 				echo  "<div class='color'>" . $item2->description . "</div><br>";
 				
-				echo  "<div class='color'>" . $item2->pubDate . "</div><br><br>";
+				echo  "<div class='color2'>" . $item2->pubDate . "</div><br><br>";
 
 				echo "</div>"; //einde volledige blok
 			}               
