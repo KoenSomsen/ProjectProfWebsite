e4733fdcc612984b901739a20ac5a2eec6252d2a
Wesley <wj.schoonbeek@gmail.com>
2019-10-17 09:40:00 +0200

    footer generic gemaakt

--- galerij.php
+++ galerij.php
@@ -35,19 +35,7 @@
     </div>
   </div>
   <!-- end main page row -->
-  <!-- Footer -->
-  <div class="row footer bg-midgreen fnt-white">
-    <div class="col-3">
-      <!-- Lege placeholder -->
-    </div>
-    <div class="col-6">
-      Copyright 2019 Amazon hogeschool
-    </div>
-    <div class="col-3">
-      <!-- Lege placeholder -->
-    </div>
-  </div>
-  <!-- end footer -->
+  <?php include 'partials/footer.php'; ?>
 
   <script src="scripts/index.js"></script>
 </body>
