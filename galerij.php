7ddb28bdb179ff31b433a3cee9708a185f252730
Wesley <wj.schoonbeek@gmail.com>
2019-10-18 12:42:48 +0200

    Image uploader gefixt

--- galerij.php
+++ galerij.php
@@ -7,12 +7,39 @@
       RSS-FEED
     </div>
     <div class="col-10 content">
+        <?php
+        if($_GET["error"]) {
+          $error = "";
+          switch($_GET["error"]) {
+            case "noimg":
+              $error = "Er is geen image gevonden";
+              break;
+            case "fileexists":
+              $error = "Er is al een bestand met het zelfde naam";
+              break;
+            case "filesize":
+              $error = "Het bestand mag niet groter zijn dan 2.5mb";
+              break;
+            case "filetype":
+              $error = "Het bestand is geen .png, .jpeg, .jpg of .gif";
+              break;
+          }
+          ////error
+          //noimg
+          //fileexists
+          //filesize
+          //filetype
+          //success
+          //uploadtrue
 
+        }
+
+        ?>
         <div class="row">
           <div class="col-2">
             <form action="php/uploadimage.php" method="POST" enctype="multipart/form-data">
               <input type="file" name="image" />
-              <input type="submit"/>
+              <input type="submit" name="submit"/>
             </form>
           </div>
         </div>
--- php/uploadimage.php
+++ php/uploadimage.php
@@ -1,8 +1,56 @@
 <?php
 
+$target_dir = "../images/galerij/"; 
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-var_dump($_FILES['image']);
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if($check !== false) {
+        $uploadOk = 1;
+        //image uploaded succesfully.
+    } else {
+        header("Location: ../galerij.php?error=noimg");
+        $uploadOk = 0;
+    }
 
-//phpinfo();
+    
+} else {
+    //Submit is not set, user should not be on this page!
+    header("Location: ../galerij.php");
+}
+
+
+if(file_exists($target_file)) {
+    //File already exists
+    echo "File alrdy exists";
+    header("Location: ../galerij.php?error=fileexists");
+} else if($_FILES["image"]["size"] > 35000 ) {
+    header("Location: ../galerij.php?error=filesize");
+    echo "img too large";
+} else if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+    //Only JPG, JPEG, PNG & Gifs are allowed.
+    echo "only jpg jpeg png gif";
+    header("Location: ../galerij.php?error=filetype");
+} else {
+    //User is allowed to upload.
+    if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+        echo basename($_FILES["image"]["name"]). "has been uploaded";
+        header("Location: ../galerij.php?success=uploadtrue");
+    }
+}
+
+//error
+//noimg
+//fileexists
+//filesize
+//filetype
+//success
+//uploadtrue
+
+
+
+
 
 ?>
