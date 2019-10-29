e9ff5d7037183e337cbdbc83951e0abce7aa24a7
Sydney Minnaar <sydney.minnaar@student.nhlstenden.com>
2019-10-29 12:56:37 +0100

    Removed empty div

--- partials/rss.php
+++ partials/rss.php
@@ -3,16 +3,13 @@
 		$feed  = "http://feeds.feedburner.com/tweakers/mixed";
 		$xml = simplexml_load_file($feed);
 		$xml2 = simplexml_load_file($feed);
-		foreach ($xml->channel->item as $item2){
-			echo "<div class='filler'>"; // volledige blok
-
+		foreach ($xml->channel->item as $item2)
+		{
 			echo "<div class='kleur-fontSize'>" .$item2->title . "</div><br>";
                       
 			echo  "<div class='color'>" . $item2->description . "</div><br>";
-				
+			
 			echo  "<div class='color2'>" . $item2->pubDate . "</div>";
-
-			echo "</div>"; //einde volledige blok
 		}               
 	?>
 </div>
