1bb56686389f2c382b2cc5cc3d0a904709d22a13
Sydney Minnaar <sydney.minnaar@student.nhlstenden.com>
2019-10-29 12:41:42 +0100

    uploadimage.php now meets the php conventions

--- php/uploadimage.php
+++ php/uploadimage.php
@@ -3,60 +3,67 @@
 $target_dir = "../images/galerij/"; 
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
 
-
-
-
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-if(isset($_POST["submit"])) {
+if(isset($_POST["submit"]))
+{
     $check = getimagesize($_FILES["image"]["tmp_name"]);
-    if($check !== false) {
+    if($check !== false)
+	{
         $uploadOk = 1;
         //image uploaded succesfully.
-    } else {
+    }
+	else
+	{
         header("Location: ../galerij.php?error=noimg");
         $uploadOk = 0;
-    }
-
-    
-} else {
+    }    
+}
+else
+{
     //Submit is not set, user should not be on this page!
     header("Location: ../galerij.php");
 }
 
 
-if(file_exists($target_file)) {
+if(file_exists($target_file))
+{
     //File already exists
     echo "File alrdy exists";
     header("Location: ../galerij.php?error=fileexists");
-} else if($_FILES["image"]["size"] > 35000 ) {
+}
+else if($_FILES["image"]["size"] > 35000 )
+{
     header("Location: ../galerij.php?error=filesize");
     echo "img too large";
-} else if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+}
+else if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif")
+{
     //Only JPG, JPEG, PNG & Gifs are allowed.
     echo "only jpg jpeg png gif";
     header("Location: ../galerij.php?error=filetype");
-} else {
+}
+else
+{
     //User is allowed to upload.
-    if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+    if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file))
+	{
         echo basename($_FILES["image"]["name"]). "has been uploaded";
         header("Location: ../galerij.php?success=uploadtrue");
     }
 }
 
-function generateRandomString($length = 15) {
+function generateRandomString($length = 15)
+{
     $characters = '-_0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
+    for ($i = 0; $i < $length; $i++)
+	{
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
 }
-
-
-
-
 
 ?>
